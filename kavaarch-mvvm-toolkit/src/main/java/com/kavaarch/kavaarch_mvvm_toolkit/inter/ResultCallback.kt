a70810ab1955719d81ddb4dc849e78f2f28a0ca7
Ankit Maurya <am1329489@gmail.com>
2025-08-06 15:46:16 +0530

    Final commit

--- kavaarch-mvvm-toolkit/src/main/java/com/kavaarch/kavaarch_mvvm_toolkit/inter/ResultCallback.kt
+++ kavaarch-mvvm-toolkit/src/main/java/com/kavaarch/kavaarch_mvvm_toolkit/inter/ResultCallback.kt
@@ -1,10 +1,14 @@
 package com.kavaarch.kavaarch_mvvm_toolkit.inter
 
+import androidx.annotation.Keep
+
+@Keep
 interface ResultCallback {
     fun onResponse(response: Any?)
     fun onError(error: Any?)
 }
 
+@Keep
 interface UploadProgressCallback {
     fun onProgress(progress: Int)
 }
